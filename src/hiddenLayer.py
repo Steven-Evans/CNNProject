8e3a7321921723569f9608eaa5e6b52849547215
Steven Evans <stevene@sfu.ca>
2014-11-20 17:16:40 -0800

    Updated hidden layer activation function to sigmoid as its our base case for an activation function

--- src/hiddenLayer.py
+++ src/hiddenLayer.py
@@ -4,7 +4,7 @@ import theano
 import theano.tensor as T
 
 class HiddenLayer(object):
-    def __init__(self, rng, input, n_in, n_out, W=None, b=None, activation=T.tanh):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None, activation=T.sigmoid):
         
         """
         Typical hidden layer of a MLP: units are fully-connected and have
